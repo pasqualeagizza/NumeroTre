f852fe9a783b96e19650b6deb7db2c1b978ee337
Pasquale Agizza <pasqualeagizza@protonmail.com>
2021-10-04 23:02:58 +0200

    Added CardView component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContainerComponent } from './container/container.component';
+import { CardViewComponent } from './card-view/card-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ContainerComponent
+    ContainerComponent,
+    CardViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/card-view/card-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'NumeroTre-card-view',
+  templateUrl: './card-view.component.html',
+  styleUrls: ['./card-view.component.css']
+})
+export class CardViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
