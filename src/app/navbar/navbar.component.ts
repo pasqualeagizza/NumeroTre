88599b49b95de57ebdf84fba945671264c7a6bff
Pasquale Agizza <pasqualeagizza@protonmail.com>
2021-10-04 22:24:47 +0200

    Created Navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'NumeroTre-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
