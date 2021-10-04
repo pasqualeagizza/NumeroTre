bbd346255b1f7b899f3a883caa19990288560cd9
Pasquale Agizza <pasqualeagizza@protonmail.com>
2021-10-04 23:03:26 +0200

    Added ListView component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContainerComponent } from './container/container.component';
 import { CardViewComponent } from './card-view/card-view.component';
+import { ListViewComponent } from './list-view/list-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ContainerComponent,
-    CardViewComponent
+    CardViewComponent,
+    ListViewComponent
   ],
   imports: [
     BrowserModule
